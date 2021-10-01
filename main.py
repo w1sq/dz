56f465570bc306a867026be1b198a87c134e6287
w1sq <artem.kokorev2005@yandex.ru>
2021-10-01 18:42:07 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,116 @@
+# coding=utf8
+import asyncio
+from asyncio import events
+import logging
+import aiogram.utils.markdown as fmt
+from datetime import datetime
+from aiogram import Bot, Dispatcher, executor,types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import InputTextMessageContent, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultPhoto
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import Text
+from aiogram.types.bot_command import BotCommand
+from aiogram.types.inline_query_result import InlineQueryResultArticle
+import requests
+from bs4 import BeautifulSoup
+
+def generate_keyboard (*answer):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    temp_buttons = []
+    for i in answer:
+        temp_buttons.append(KeyboardButton(text=i))
+    keyboard.add(*temp_buttons)
+    return keyboard
+
+def generate_inline_keyboard (*answer):
+    keyboard = InlineKeyboardMarkup()
+    temp_buttons = []
+    for i in answer:
+        temp_buttons.append(InlineKeyboardButton(text=str(i[0]), callback_data=i[1]))
+    keyboard.add(*temp_buttons)
+    return keyboard
+
+with open('key.txt','r') as file:
+    API_KEY = file.readline()
+logging.basicConfig(level=logging.INFO, filename='botlogs.log')
+bot = Bot(token=API_KEY)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=MemoryStorage())
+print('Bot started')
+
+class GetNumber(StatesGroup):
+    rus_ege_sdamgia_ru = State()
+
+website_keyboard = ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True).row(KeyboardButton('Сдам Гиа'))
+sdamgia_subjects_keyboard = InlineKeyboardMarkup(resize_keyboard=True).row(InlineKeyboardButton(text='Русский',callback_data='sdamgia_rus'))
+sdamgia_exams_keyboard = InlineKeyboardMarkup(resize_keyboard=True).row(InlineKeyboardButton(text='ЕГЭ',callback_data='sdamgia_ege'))
+
+@dp.message_handler(state='*', commands='❌Отмена')
+@dp.message_handler(state='*', commands='cancel')
+@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
+async def cancel_handler(state: FSMContext):
+    current_state = await state.get_state()
+    logging.info('Cancelling state %r', current_state)
+    await state.finish()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Бот пока находится в разработке, не все предметы и сайты еще добавлены.\n\nВыберите желаемый сайт:',reply_markup=website_keyboard)
+
+@dp.message_handler(text='Сдам Гиа')
+async def search_serial(message):
+    await message.delete()
+    await message.answer('Выберите экзамен: ',reply_markup=sdamgia_exams_keyboard)
+
+@dp.message_handler(state=GetNumber.rus_ege_sdamgia_ru)
+async def process_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        try:
+            msg = int(message.text)
+            webpage = requests.get(f'https://rus-ege.sdamgia.ru/problem?id={message.text}').text
+            if 'Такого задания не существует.' not in webpage:
+                soup = BeautifulSoup (webpage, 'html.parser')
+                keyb =  InlineKeyboardMarkup(resize_keyboard=True).row(InlineKeyboardButton(text='Вернуться к выбору предмета',callback_data='sdamgia_ege')).row(InlineKeyboardButton(text='Вернуться к выбору экзамена',callback_data='choose_exam')).row(InlineKeyboardButton(text='Ввести номер нового задания',callback_data='sdamgia_rus'))
+                await message.answer(soup.find("div", class_="answer").text,reply_markup=keyb)
+                data.state = None
+            else:
+                await message.answer('Задания не найдено')
+                data.state = None
+        except ValueError:
+            await message.answer('Неправильный формат ввода, попробуйте снова\nвведите cancel для отмены')
+
+async def sdamgia_rus(call,*args):
+    message = call.message
+    await GetNumber.rus_ege_sdamgia_ru.set()
+    await message.answer('Введите номер задания')
+    await message.delete()
+
+async def sdamgia_ege(call,*args):
+    message = call.message    
+    await message.answer('Выберите предмет',reply_markup = sdamgia_subjects_keyboard)
+    await message.delete()
+
+async def choose_exam(call,*args):
+    message = call.message    
+    await message.answer('Выберите экзамен',reply_markup = sdamgia_subjects_keyboard)
+    await message.delete()
+
+events = {'sdamgia_rus':sdamgia_rus,
+    'sdamgia_ege':sdamgia_ege,
+    'choose_exam':choose_exam
+}
+
+@dp.callback_query_handler(lambda call: True)
+async def ans(call):
+    command = call.data.split()
+    await events[command[0]](call,*command[1:])
+
+async def main():
+    await dp.start_polling()
+
+if __name__ == '__main__':
+    asyncio.run(main())
